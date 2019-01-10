bc2840a35a4fbd7a55360c1c49bb73a1315b2ea7
Jacob Thomas <>
2019-01-10 13:40:51 -0600

    Chapter 06

---
+++ src/Chapter06/C6_1.java
@@ -0,0 +1,60 @@
+package Chapter06;
+
+import java.util.Scanner;
+
+/**
+ * Tells you if your passwords valid. ( ͡° ͜ʖ ͡°)
+ *
+ * @author Jacob Thomas
+ */
+public class C6_1 {
+
+    /**
+     * Main method
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your password: ");
+        String password = input.next();
+
+        System.out.println(isValid(password) ? "Valid Password" : "Invalid Password");
+    }
+
+    /**
+     *
+     * @param password - user input
+     * @return
+     */
+    public static boolean isValid(String password) {
+        // Atleast 8 characters
+        if (password.length() < 8) {
+            System.out.println("A password must have at least eight characters");
+            return false;
+        }
+
+        // Only letters and digits
+        for (int i = 0; i < password.length(); i++) {
+            if (!Character.isLetterOrDigit(password.charAt(i))) {
+                System.out.println("A password must consist of only letters and digits");
+                return false;
+            }
+        }
+
+        // Atleast 2 digits
+        int digits = 0;
+        for (int i = 0; i < password.length(); i++) {
+            if (!Character.isDigit(password.charAt(i))) {
+                ++digits;
+            }
+        }
+        if (digits < 2) {
+            System.out.println("A password must contain at least two digits");
+            return false;
+        }
+
+        // If all conditions are met
+        return true;
+    }
+}
---
+++ src/Chapter06/P6.java
@@ -0,0 +1,67 @@
+package Chapter06;
+
+import java.util.Scanner;
+
+/**
+ * Does some super cool money conversion.
+ *
+ * @author Jacob Thomas
+ */
+public class P6 {
+
+    /**
+     * Main method
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Euro Rate: ");
+        double euroRate = input.nextDouble();
+        System.out.print("Pound Sterling Rate: ");
+        double poundSterlingRate = input.nextDouble();
+        System.out.print("Yen Rate: ");
+        double yenRate = input.nextDouble();
+        String response = "";
+
+        do {
+            System.out.print("Please enter the number of dollars you want to convert: $");
+            double dollars = input.nextDouble();
+
+            System.out.print("Enter \"E\" to buy Euros, \"P\" to buy Pounds or \"Y\" to buy Yen:");
+            String code = input.next().toUpperCase();
+            switch (code) {
+                case "E":
+                    System.out.printf("%.2f dollars = %.2f Euros.\n", dollars, convert(dollars, euroRate));
+                    break;
+                case "P":
+                    System.out.printf("%.2f dollars = %.2f Pound Sterlings.\n", dollars, convert(dollars, poundSterlingRate));
+                    break;
+                case "Y":
+                    System.out.printf("%.2f dollars = %.2f Yen.\n", dollars, convert(dollars, yenRate));
+                    break;
+            }
+
+            do {
+                System.out.print("Are there more conversions to perform? ");
+                response = input.next().toLowerCase();
+            } while (!response.equals("yes") && !response.equals("no"));
+
+        } while (response.equals("yes"));
+
+    }
+
+    /**
+     *
+     * @param dollars - Stands for USD
+     * @param exchangeRate - Stands for the exchange rate inputed by the user
+     * @return
+     */
+    public static double convert(double dollars, double exchangeRate) {
+        if (dollars > 100) {
+            return 0.95 * dollars * exchangeRate;
+        } else {
+            return 0.90 * dollars * exchangeRate;
+        }
+    }
+}
